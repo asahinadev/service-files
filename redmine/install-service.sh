2deb7e50454628965de662be4282b9f499c10a3e
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-10 16:46:00 +0900

    Create install-service.sh

---
+++ redmine/install-service.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+DIR=${DIR:/var/redmine/current}
+
+cat << EOS > /usr/lib/systemd/system/redmine-unicorn.service
+[Unit]
+Description=Redmine Unicorn Server
+After=mysqld
+
+[Service]
+WorkingDirectory=${DIR}
+Environment=RAILS_ENV=production
+SyslogIdentifier=redmine-unicorn
+PIDFile=${DIR}/tmp/pids/unicorn.pid
+
+ExecStart=/usr/local/bin/bundle exec "unicorn_rails -c config/unicorn.rb -E production" 
+ExecStop=/usr/bin/kill -QUIT $MAINPID
+ExecReload=/bin/kill   -USR2 $MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOS

d50681aa2723bdca658b45e597eae1600496f7f9
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-10 18:26:15 +0900

    Update install.sh

--- apache-tomcat/install.sh
+++ apache-tomcat/install.sh
@@ -1,28 +1,22 @@
-#!/bin/sh 
+#!/bin/sh
 
-dnf -y install wget
+# 環境設定
+MJ=${MJ:="9"}
+VER=${VER:="9.0.34"}
+TAR="apache-tomcat-${VER}.tar.gz"
+DIR="apache-tomcat-${VER}"
+URL=${URL:="http://archive.apache.org/dist/tomcat/tomcat-${MJ}/v${VER}/bin/apache-tomcat-${VER}.tar.gz"}
+HTTP=${HTTP:=9301}
+AJP=${AJP:=9300}
 
-SRC_NAME=apache-tomcat
-DES_PATH=/usr/local/apache-tomcat
-SRC_PATH=/usr/local/src/apache-tomcat
+mkdir -p /usr/local/tomcat
+cd       /usr/local/tomcat
 
-mkdir -p $SRC_PATH
-mkdir -p $DES_PATH
+# ダウンロード & インストール
+wget $URL
+tar -xzf ${TAR}
+ln -snf  ${DIR} current
+rm -f ${TAR}
 
-cd       $DES_PATH
-
-wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.34/bin/apache-tomcat-9.0.34-fulldocs.tar.gz
-
-tar -xzvf apache-tomcat-9.0.34-fulldocs.tar.gz
-
-cp -pr $SRC_PATH/apache-tomcat-9.0.34 $DES_PATH/9.0.34
-
-alternatives --remove-all                $SRC_NAME
-alternatives --install $DES_PATH/current $SRC_NAME $DES_PATH/9.0.34 1
-
-# service 登録
-cp -p $SRC_PATH/apache-tomcat.service /usr/lib/systemd/system/
-cp -p $SRC_PATH/apache-tomcat          /etc/sysconfig/
-
-systemctl enable $SRC_NAME
-systemctl start  $SRC_NAME
+sed -i "s/port=\"8080\"/port=\"${PORT}\"/g" ${DIR}/conf/server.xml | grep port
+sed -i "s/port=\"8009\"/port=\"${AJP}\"/g"  ${DIR}/conf/server.xml | grep port

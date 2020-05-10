bf2468def399e14cafd7f237cb9431204def7af3
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-10 20:47:46 +0900

    Update set-alternatives.sh

--- java/set-alternatives.sh
+++ java/set-alternatives.sh
@@ -4,34 +4,34 @@ VERSION=${VERSION:=15}
 jdk="jdk-${VERSION}"
 
 alternatives --install /usr/bin/java  java  /usr/lib/jvm/${jdk}/bin/java ${VERSION} \
- --slave /usr/bin/jaotc jaotc /usr/lib/jvm/${jdk}/bin/jjaotc \
- --slave /usr/bin/jar jar /usr/lib/jvm/${jdk}/bin/jjar \
- --slave /usr/bin/jarsigner jarsigner /usr/lib/jvm/${jdk}/bin/jjarsigner \
- --slave /usr/bin/javac javac /usr/lib/jvm/${jdk}/bin/jjavac \
- --slave /usr/bin/javadoc javadoc /usr/lib/jvm/${jdk}/bin/jjavadoc \
- --slave /usr/bin/javap javap /usr/lib/jvm/${jdk}/bin/jjavap \
- --slave /usr/bin/jcmd jcmd /usr/lib/jvm/${jdk}/bin/jjcmd \
- --slave /usr/bin/jconsole jconsole /usr/lib/jvm/${jdk}/bin/jjconsole \
- --slave /usr/bin/jdb jdb /usr/lib/jvm/${jdk}/bin/jjdb \
- --slave /usr/bin/jdeprscan jdeprscan /usr/lib/jvm/${jdk}/bin/jjdeprscan \
- --slave /usr/bin/jdeps jdeps /usr/lib/jvm/${jdk}/bin/jjdeps \
- --slave /usr/bin/jhsdb jhsdb /usr/lib/jvm/${jdk}/bin/jjhsdb \
- --slave /usr/bin/jimage jimage /usr/lib/jvm/${jdk}/bin/jjimage \
- --slave /usr/bin/jinfo jinfo /usr/lib/jvm/${jdk}/bin/jjinfo \
- --slave /usr/bin/jjs jjs /usr/lib/jvm/${jdk}/bin/jjjs \
- --slave /usr/bin/jlink jlink /usr/lib/jvm/${jdk}/bin/jjlink \
- --slave /usr/bin/jmap jmap /usr/lib/jvm/${jdk}/bin/jjmap \
- --slave /usr/bin/jmod jmod /usr/lib/jvm/${jdk}/bin/jjmod \
- --slave /usr/bin/jps jps /usr/lib/jvm/${jdk}/bin/jjps \
- --slave /usr/bin/jrunscript jrunscript /usr/lib/jvm/${jdk}/bin/jjrunscript \
- --slave /usr/bin/jshell jshell /usr/lib/jvm/${jdk}/bin/jjshell \
- --slave /usr/bin/jstack jstack /usr/lib/jvm/${jdk}/bin/jjstack \
- --slave /usr/bin/jstat jstat /usr/lib/jvm/${jdk}/bin/jjstat \
- --slave /usr/bin/jstatd jstatd /usr/lib/jvm/${jdk}/bin/jjstatd \
- --slave /usr/bin/keytool keytool /usr/lib/jvm/${jdk}/bin/jkeytool \
- --slave /usr/bin/pack200 pack200 /usr/lib/jvm/${jdk}/bin/jpack200 \
- --slave /usr/bin/rmic rmic /usr/lib/jvm/${jdk}/bin/jrmic \
- --slave /usr/bin/rmid rmid /usr/lib/jvm/${jdk}/bin/jrmid \
- --slave /usr/bin/rmiregistry rmiregistry /usr/lib/jvm/${jdk}/bin/jrmiregistry \
- --slave /usr/bin/serialver serialver /usr/lib/jvm/${jdk}/bin/jserialver \
- --slave /usr/bin/unpack200 unpack200 /usr/lib/jvm/${jdk}/bin/junpack200
+ --slave /usr/bin/jaotc jaotc /usr/lib/jvm/${jdk}/bin/jaotc \
+ --slave /usr/bin/jar jar /usr/lib/jvm/${jdk}/bin/jar \
+ --slave /usr/bin/jarsigner jarsigner /usr/lib/jvm/${jdk}/bin/jarsigner \
+ --slave /usr/bin/javac javac /usr/lib/jvm/${jdk}/bin/javac \
+ --slave /usr/bin/javadoc javadoc /usr/lib/jvm/${jdk}/bin/javadoc \
+ --slave /usr/bin/javap javap /usr/lib/jvm/${jdk}/bin/javap \
+ --slave /usr/bin/jcmd jcmd /usr/lib/jvm/${jdk}/bin/jcmd \
+ --slave /usr/bin/jconsole jconsole /usr/lib/jvm/${jdk}/bin/jconsole \
+ --slave /usr/bin/jdb jdb /usr/lib/jvm/${jdk}/bin/jdb \
+ --slave /usr/bin/jdeprscan jdeprscan /usr/lib/jvm/${jdk}/bin/jdeprscan \
+ --slave /usr/bin/jdeps jdeps /usr/lib/jvm/${jdk}/bin/jdeps \
+ --slave /usr/bin/jhsdb jhsdb /usr/lib/jvm/${jdk}/bin/jhsdb \
+ --slave /usr/bin/jimage jimage /usr/lib/jvm/${jdk}/bin/jimage \
+ --slave /usr/bin/jinfo jinfo /usr/lib/jvm/${jdk}/bin/jinfo \
+ --slave /usr/bin/jjs jjs /usr/lib/jvm/${jdk}/bin/jjs \
+ --slave /usr/bin/jlink jlink /usr/lib/jvm/${jdk}/bin/jlink \
+ --slave /usr/bin/jmap jmap /usr/lib/jvm/${jdk}/bin/jmap \
+ --slave /usr/bin/jmod jmod /usr/lib/jvm/${jdk}/bin/jmod \
+ --slave /usr/bin/jps jps /usr/lib/jvm/${jdk}/bin/jps \
+ --slave /usr/bin/jrunscript jrunscript /usr/lib/jvm/${jdk}/bin/jrunscript \
+ --slave /usr/bin/jshell jshell /usr/lib/jvm/${jdk}/bin/jshell \
+ --slave /usr/bin/jstack jstack /usr/lib/jvm/${jdk}/bin/jstack \
+ --slave /usr/bin/jstat jstat /usr/lib/jvm/${jdk}/bin/jstat \
+ --slave /usr/bin/jstatd jstatd /usr/lib/jvm/${jdk}/bin/jstatd \
+ --slave /usr/bin/keytool keytool /usr/lib/jvm/${jdk}/bin/keytool \
+ --slave /usr/bin/pack200 pack200 /usr/lib/jvm/${jdk}/bin/pack200 \
+ --slave /usr/bin/rmic rmic /usr/lib/jvm/${jdk}/bin/rmic \
+ --slave /usr/bin/rmid rmid /usr/lib/jvm/${jdk}/bin/rmid \
+ --slave /usr/bin/rmiregistry rmiregistry /usr/lib/jvm/${jdk}/bin/rmiregistry \
+ --slave /usr/bin/serialver serialver /usr/lib/jvm/${jdk}/bin/serialver \
+ --slave /usr/bin/unpack200 unpack200 /usr/lib/jvm/${jdk}/bin/unpack200

10fa4a83df16fff03a44a9561139b25153ddd022
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-10 14:25:15 +0900

    Update install.sql

--- redmine/install.sql
+++ redmine/install.sql
@@ -3,4 +3,4 @@
 CREATE DATABASE redmine CHARACTER SET utf8mb4;
 CREATE USER 'redmine'@'localhost' IDENTIFIED WITH mysql_native_password;
 ALTER USER 'redmine'@'localhost' identified BY 'redmine';
-GRANT ALL ON *.* TO 'redmine'@'localhost' with grant option;
+GRANT ALL ON redmine.* TO 'redmine'@'localhost' with grant option;

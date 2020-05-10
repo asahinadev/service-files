4951d6e457d2d0edfa54b84240bc93ba80d37ce1
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-10 14:31:26 +0900

    Update install.sql

--- redmine/install.sql
+++ redmine/install.sql
@@ -1,6 +1,16 @@
 -- MySQL 8.0
 
+-- データベース作成
 CREATE DATABASE redmine CHARACTER SET utf8mb4;
+
+-- ユーザー作成
 CREATE USER 'redmine'@'localhost' IDENTIFIED WITH mysql_native_password;
+
+-- パスワード設定
 ALTER USER 'redmine'@'localhost' identified BY 'redmine';
+
+-- 権限追加
 GRANT ALL ON redmine.* TO 'redmine'@'localhost' with grant option;
+FLUSH PRIVILEGES;
+
+-- END

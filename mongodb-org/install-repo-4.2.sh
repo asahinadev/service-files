c9fa64085864ef906d28cbbee1ef744e9338c64f
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-10 21:33:12 +0900

    Update install-repo-4.2.sh

--- mongodb-org/install-repo-4.2.sh
+++ mongodb-org/install-repo-4.2.sh
@@ -3,7 +3,7 @@
 cat << EOS > /etc/yum.repos.d/mongodb-org-4.2.repo
 [mongodb-org-4.2]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=0
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc

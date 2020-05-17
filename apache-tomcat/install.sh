de8e6775b83ac5aea2595c816ad465d420e9d230
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-17 16:31:09 +0900

    Update install.sh

--- apache-tomcat/install.sh
+++ apache-tomcat/install.sh
@@ -3,22 +3,17 @@
 # 環境設定
 MJ=${MJ:="9"}
 VER=${VER:="9.0.34"}
+
 TAR="apache-tomcat-${VER}.tar.gz"
 DIR="apache-tomcat-${VER}"
 URL=${URL:="http://archive.apache.org/dist/tomcat/tomcat-${MJ}/v${VER}/bin/apache-tomcat-${VER}.tar.gz"}
-HTTP=${HTTP:=9301}
-AJP=${AJP:=9300}
 
 mkdir -p /usr/local/tomcat
 cd       /usr/local/tomcat
 
 # ダウンロード & インストール
 wget $URL
-tar -xzf ${TAR}
-ln -snf  ${DIR} current
-rm -f ${TAR}
+tar -xzfv ${TAR}
+ln -snf   ${DIR} current
+rm -f     ${TAR}
 
-sed -i "s/port=\"8080\"/port=\"${PORT}\"/g" ${DIR}/conf/server.xml 
-sed -i "s/port=\"8009\"/port=\"${AJP}\"/g"  ${DIR}/conf/server.xml 
-sed -i '$ i \ \ \ \ <Valve className="org.apache.catalina.valves.ErrorReportValve" showReport="false" showServerInfo="fa
-lse"/>' context.xml

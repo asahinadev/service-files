2b7be85f498decbc932846b18c248ca682cd73f5
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-24 23:12:03 +0900

    Update install.sh

--- docker/centos7/install.sh
+++ docker/centos7/install.sh
@@ -7,6 +7,7 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum -y install docker-ce docker-ce-cli containerd.io
 
 wget https://github.com/docker/compose/releases/download/1.25.5/docker-compose-Linux-x86_64 -O /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 systemctl enable docker
 systemctl start docker
